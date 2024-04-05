61f3c2765bddbe7d814a321c8a81690562efc3d2
satyam-seth <satyam1998.1998@gmail.com>
2024-04-05 23:46:35 +0530

    use ip api to get user local ip info

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 [Idea Link](https://www.geeksforgeeks.org/ip-address-finder-app-using-reactjs/)
+
+[IP Address Finder API](https://ipapi.co/)
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
+import { useState, useEffect } from "react";
+
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://ipapi.co/json/")
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error));
+  }, []);
 
   return (
     <>
       <h1>IP Address Finder</h1>
+      <div>
+        {data === null ? (
+          <p>Loading...</p>
+        ) : (
+          <table border={1}>
+            <tbody>
+              {Object.keys(data).map((key, index) => (
+                <tr key={index}>
+                  <td>{key}</td>
+                  <td>{data[key]}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
